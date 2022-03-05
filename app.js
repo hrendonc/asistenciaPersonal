b69c0b0bd57f005ecaaaa1c285ef998f57a4515c
Hector Rendon <flakonet@msn.com>
2022-03-05 07:26:32 -0600

    Solucionado el problema de salidas

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-let registro = [
-    {
-        fecha: 2022228,
-        entrada: "8:00",
-        salida: "15:30"
-    }
-]
+let registro = []
 
 const btnRegistro = document.getElementById('btnRegistro')
 const tBody = document.getElementById('regs')
@@ -20,11 +14,11 @@ const pintarRegs = ()=>{
     tBody.innerHTML=''
     
     registro.map(dato=>{
-        if(dato.salida == undefined) dato.salida="Sin registrar"
+        //if(dato.salida == undefined) dato.salida="Sin registrar"
         const lista = document.createElement('tr')
         const dato1 = document.createTextNode(`${dato.fecha}`)
         const dato2 = document.createTextNode(`${dato.entrada}`)
-        const dato3 = document.createTextNode(`${dato.salida}`)
+        const dato3 = document.createTextNode(`${dato.salida ? dato.salida : "Sin registrar"}`)
         const item1 = document.createElement('td')
         const item2 = document.createElement('td')
         const item3 = document.createElement('td')
@@ -65,20 +59,20 @@ const addRegistro = ()=>{
     registro.map(x=>{
         if (x.fecha == addFecha) {
             existe = true
-            if (!x.salida && (data.hora == 15) && (data.minuto >= 30 && data.minuto <= 59)) { //Salida: 15:30-59
+            if (!x.salida && (data.hora == 7) && (data.minuto >= 26 && data.minuto <= 26)) { //Salida: 15:30-59
                 x.salida = addHora
                 alert('Se registro la Salida!')
             }else{
                 if(x.entrada){
-                    if (data.hora == 8 && (data.minuto >= 0 && data.minuto <= 30)) { //Entrada: 8:0-30
+                    if (data.hora == 7 && (data.minuto >= 24 && data.minuto <= 24)) { //Entrada: 8:0-30
                         alert('Su Entrada ya está registrada!')
                         return
                     }
                 }
-                if(x.salida != "Sin registrar") {
+                 if(x.salida) {
                     alert('Ya está registrada su salida!')
-                }else{
-                    alert('El registro de SALIDA está fuera de tiempo!')
+                 }else{
+                    alert('El registro de SALIDA1 está fuera de tiempo!')
                 }
 
             }         
@@ -96,15 +90,16 @@ const addRegistro = ()=>{
             salida: addHora
         }
 
-        if (data.hora == 8 && (data.minuto >= 0 && data.minuto <= 30)) { //Entrada: 8:0-30
+        if (data.hora == 7 && (data.minuto >= 24 && data.minuto <= 24)) { //Entrada: 8:0-30
             registro.push(addEntrada)
             alert('Se registro la Entrada!')
             return
         }else{
             alert('El registro de ENTRADA está fuera de tiempo!')
+            return
         }
 
-        if (data.hora == 15 && (data.minuto >= 30 && data.minuto <= 59)) { //Salida: 15:30-59
+        if (data.hora == 7 && (data.minuto >= 26 && data.minuto <= 26)) { //Salida: 15:30-59
             registro.push(addSalida)
             alert('Se registró la Salida, pero no la Entrada!')
         }else{
